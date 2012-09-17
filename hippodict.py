dfc62c08eb93e8ddd5767d2faf01222dd2dafb4b
Tripta Gupta <gupta.tripta+github@gmail.com>
2012-09-16 18:56:37 -0700

    editing filenames

--- hippodict.py
+++
@@ -1,9 +0,0 @@
-responses = {
-    'Layer2and3' : "You are currently in between layer 2 and 3 of the entorhinal cortex. You have the option of traveling in either layer. Which layer do you want to walk along?", 
-    'DentateGyrus' : """Ah, the dentate gyrus. Good pick. The dentate gyrus is one of the few regions in the adult brain where neurogenesis is thought to take place. It also plays a role in depression.""" """ There are three major types of neurons found here.""" """ Would you like to learn about them?""", 
-    'HippocampalRegionCA3' : """ Now you're in the hippocampal region CA3 or simply, the CA3.""" """ There are a few proposed behavioral functions of the CA3.""" """ Would you like to learn about one?""", 
-    'Subiculum' : """Hey, now you're in the most inferior region of the hippcampus called the subiculum. It lies between the entorhinal cortex and the CA1.""" """ Do you want to stay or leave?""", 
-    'HippocampalRegionCA1' : """Welcome to the hippocampal region CA1 or simply, CA1.""" """This region sends a large amount of output to the subiculum and some inputs back to the entorhinal cortex.""" """Which way would you like to travel?""", 
-    'EntorhinalCortex2' : """You have now, more or less, made a full loop within the connections of the hippocampus. Congratulations!""" """If you want to explore a bit more, please replay the game! Is this what you want to do?""", 
-    'StartHere' : """Hello. You're about to explore the hippocampus. """ """There will be many paths and therfore, many decisions you can make.""" """Enter 'Ready!' when prompted and you can begin your journey.""", 
-    'TryAgain' : "That doesn't make much sense. Try typing that in again."}
